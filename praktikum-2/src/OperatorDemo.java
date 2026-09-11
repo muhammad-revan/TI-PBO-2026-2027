6a3c365a3ca1b430e1cce88366cb659cc62a9d84
muhammad revan triadi <revan111550@gmail.com>
2026-09-11 12:59:22 +0700

    Operator

---
+++ praktikum-2/src/OperatorDemo.java
@@ -0,0 +1,13 @@
+public class OperatorDemo {
+    public static void main(String[] args) {
+        int a = 17, b=5;
+        System.out.println("Jumlah: " +(a+b));
+        System.out.println("selisih: "+(a-b));
+        System.out.println("kali: " +(a*b));
+        System.out.println("bagi: " +(a/b));
+        System.out.println("hasil bagi: "+(a%b));
+
+
+    }
+
+}
