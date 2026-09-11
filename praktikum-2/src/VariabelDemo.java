d72864e99b64b01da9c441515fdfd235354bd225
muhammad revan triadi <revan111550@gmail.com>
2026-09-11 12:52:00 +0700

    Deklarasi Variabel dan Tipe Data Primitif

---
+++ praktikum-2/src/VariabelDemo.java
@@ -0,0 +1,17 @@
+public class VariabelDemo {
+    public static void main(String[] args) {
+        String nama = "Muhammad Revan Triadi";
+        int umur = 19;
+        double tinggi = 170;
+        char goldarah = 'O';
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Golongan Darah: " + goldarah);
+        System.out.println("Mahasiswa Aktif: " + mahasiswaAktif);
+
+    }
+
+}
