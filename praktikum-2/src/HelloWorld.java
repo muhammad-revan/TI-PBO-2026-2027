bde558bccee4f0113adbb56a04d352e253a73b23
muhammad revan triadi <revan111550@gmail.com>
2026-09-11 12:47:35 +0700

    Struktur Dasar Program Java

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Muhammad Revan Triadi");
+
+        System.out.print("Nama:");
+        System.out.print("Muhammad Revan Triadi");
+        System.out.println();
+        System.out.println("ini baris baru");
     }
 }
