2dc95c07a7983cc48cb070f337936143321cd77f
muhammad revan triadi <revan111550@gmail.com>
2026-09-14 19:49:20 +0700

    B.1. Struktur Dasar Switch-Case

---
+++ praktikum-3/src/SwitchDemo.java
@@ -0,0 +1,19 @@
+public class SwitchDemo {
+    public static void main(String[] args){
+        int hari =3;
+
+        switch(hari){
+            case 1:
+                System.out.println("senin");
+                break;
+            case 2:
+                System.out.println("selasa");
+                break;
+            case 3:
+                System.out.println("rabu");
+                break;
+            default:
+                System.out.println("hari tidak di kenali");
+        }
+    }
+}
